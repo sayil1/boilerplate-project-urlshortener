ead854e7c779a0235de7bba2e3bc5bed4cddc8ff
sayil1 <sundaysayil4u@gmail.com>
2024-05-03 16:42:02 +0100

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +8,72 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function (req, res) {
+  const { url: _url } = req.body;
+
+  if (_url === "") {
+    return res.json({
+      error: "invalid url",
+    });
+  }
+
+  let parsed_url;
+  const modified_url = _url.replace(
+    /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
+    ""
+  );
+
+  try {
+    parsed_url = new URL(_url);
+  } catch (err) {
+    return res.json({
+      error: "invalid url",
+    });
+  }
+
+  DNS.lookup(modified_url, (err) => {
+    if (err) {
+      return res.json({
+        error: "invalid url",
+      });
+    } else {
+      const link_exists = URLs.find((l) => l.original_url === _url);
+
+      if (link_exists) {
+        return res.json({
+          original_url: _url,
+          short_url: id,
+        });
+      } else {
+        // increment for each new valid url
+        ++id;
+
+        // object creation for entry into url
+        const url_object = {
+          original_url: _url,
+          short_url: `${id}`,
+        };
+
+        // pushing each new entry into the array
+        URLs.push(url_object);
+
+        // return the new entry created
+        return res.json({
+          original_url: _url,
+          short_url: id,
+        });
+      }
+    }
+  });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
